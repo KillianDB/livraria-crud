633e1320cde364b70368dd44b369278fcaed8952
Julio Cesar Bueno <buenojc@outlook.com>
2023-03-15 23:16:56 -0300

    add: setup ejs

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const router = require('./router')
 
-
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.use(router)
+
 
 app.listen(3000, () => {
     console.log('O servidor está rodando')
